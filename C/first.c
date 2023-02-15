2ad7c8b8611555cf8ec2cd0dd94a5663c48b0789
Oscar <ttoscar@gmail.com>
2023-02-15 14:13:08 -0500

    adding C folder with hello world with variable

---
+++ C/first.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*
+    this is the basic skeleton for a C code, standalone, as a script.
+    with gcc (windows compiled, but this one i guess is pretty portable)
+    will generate an exe and when the exe is executed will show a message 
+    in the console.
+
+    consider it your standard hello world example
+*/
+
+int main(){
+   int value = 101; 
+   printf("this is a value from a var %d  \n\n", value);
+}
