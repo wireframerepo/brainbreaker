6b183e658d8ae9ea13ebe54b14726fc830b77efc
Mister X <chiquix@MacBook-Pro-de-Mister.local>
2023-02-17 03:21:00 -0500

    adding python folder to the project

---
+++ python/starter.py
@@ -0,0 +1,13 @@
+import random
+# single line comment
+"""
+multi line
+comments
+
+hello world with import for random number i guess
+"""
+
+# string.format helps you to place variables in the message
+print("hello there {}".format(random.randint(0,9)))
+
+# there you have it this is barebones basic python 3
